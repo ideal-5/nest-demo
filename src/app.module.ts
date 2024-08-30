38ff372167f7a2891d979939ac7ca8b7d9637dba
ideal <ide_al@163.com>
2024-08-30 11:53:14 +0800

    feat: 创建posts，暂时链接上了数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'ideal',
+      database: 'blog',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // 自动载入的模型将同步
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
